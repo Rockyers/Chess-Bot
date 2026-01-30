dbddaf8f295f19ebaa42f991d368609bc5ae72d4
Rockyers <ethan@yocom.org>
2026-01-29 23:40:58 -0800

    Holy bug fix

--- Chess-Challenge/src/My Bot/EthanBot.cs
+++ Chess-Challenge/src/My Bot/EthanBot.cs
@@ -8,6 +8,9 @@ public class EthanBot : IChessBot
     // Transposition Table
     private const int TTSize = 1 << 20; // ~1M entries
     private readonly TTEntry[] _tt = new TTEntry[TTSize];
+    
+    // Killer moves, 2 per depth
+    private readonly Move[,] _killerMoves = new Move[32, 2];
 
     private enum TTFlag { Exact, LowerBound, UpperBound }
 
@@ -22,14 +25,15 @@ public class EthanBot : IChessBot
     
     public Move Think(Board board, Timer timer)
     {
-        var maxScore = int.MinValue;
-        Move? bestMove = null;
-        
         var (zKey, _, entry) = GetZobrist(board);
 
         Span<Move> moves = stackalloc Move[128];
+        board.GetLegalMovesNonAlloc(ref moves);
         GetAndOrderMoves(board, moves, entry, zKey);
 
+        var maxScore = int.MinValue;
+        var bestMove = moves[0];
+        
         foreach (var move in moves)
         {
             board.MakeMove(move);
@@ -41,8 +45,8 @@ public class EthanBot : IChessBot
             maxScore = score;
             bestMove = move;
         }
-        
-        return bestMove ?? Move.NullMove;
+
+        return bestMove;
     }
 
     private int Search(int depth, Board board, int alpha, int beta)
@@ -74,7 +78,10 @@ public class EthanBot : IChessBot
             }
 
             if (alpha >= beta)
+            {
+                UpdateKillerMoves(entry.BestMove, board.PlyCount);
                 return entry.Score;
+            }
         }
         
         if (depth == 0)
@@ -84,12 +91,14 @@ public class EthanBot : IChessBot
         var bestMove = Move.NullMove;
         
         Span<Move> moves = stackalloc Move[128];
+        board.GetLegalMovesNonAlloc(ref moves);
         GetAndOrderMoves(board, moves, entry, zKey);
 
         foreach (var move in moves)
         {
+            var ply = board.PlyCount;
             board.MakeMove(move);
-            var score = Negate(Search(depth - 1, board, Negate(beta), Negate(alpha)));            
+            var score = Negate(Search(depth - 1, board, Negate(beta), Negate(alpha)));
             board.UndoMove(move);
             
             if (score > maxScore)
@@ -102,7 +111,10 @@ public class EthanBot : IChessBot
                 alpha = score;
 
             if (alpha >= beta)
+            {
+                UpdateKillerMoves(move, ply);
                 break;
+            }
         }
 
         var flag = (maxScore <= originalAlpha, maxScore >= beta) switch
@@ -128,54 +140,78 @@ public class EthanBot : IChessBot
         return maxScore;
     }
 
+    private void UpdateKillerMoves(Move move, int ply)
+    {
+        if (move.IsCapture)
+            return;
+
+        if (_killerMoves[ply, 0] == move) return;
+        
+        _killerMoves[ply, 1] = _killerMoves[ply, 0];
+        _killerMoves[ply, 0] = move;
+    }
+
     private void GetAndOrderMoves(Board board, Span<Move> moves, TTEntry ttEntry, ulong zKey)
     {
-        board.GetLegalMovesNonAlloc(ref moves);
+        var hasTTMove = ttEntry.Key == zKey;
 
-        TTEntry? entry = ttEntry;
-        if (entry.Value.Key != zKey || entry.Value.Depth == 0)
-            entry = null;
-        
-        Span<int> scores = stackalloc int[moves.Length];
-        for (var i = 0; i < moves.Length; i++)
+        var writeIndex = 0;
+        if (hasTTMove && ttEntry.BestMove != Move.NullMove)
+            BubbleMoves(moves, ref writeIndex, move => move == ttEntry.BestMove);
+
+        for (var k = 0; k < 2; k++)
         {
-            var move = moves[i];
-            var score = EvaluateMove(entry, move);
-            scores[i] = score;
+            var k1 = k;
+            var killer = _killerMoves[board.PlyCount, k1];
+            
+            if (killer != Move.NullMove)
+                BubbleMoves(moves, ref writeIndex, move => move == killer);
         }
+
+        var startOfCaptures = writeIndex;
+        BubbleMoves(moves, ref writeIndex, move => move.IsCapture);
         
-        // INSERTION SORT BY SCORE
-        for (var i = 1; i < moves.Length; i++)
+        OrderCaptures(moves[startOfCaptures..writeIndex]);
+    }
+
+    private static void BubbleMoves(Span<Move> moves, ref int writeIndex, Predicate<Move> predicate)
+    {
+        for (var i = writeIndex; i < moves.Length; i++)
         {
-            var move = moves[i];
+            if (!predicate.Invoke(moves[i])) continue;
+                
+            Swap(ref moves[i], ref moves[writeIndex]);
+            writeIndex++;
+                
+            break;
+        }
+    }
+    
+    private void OrderCaptures(Span<Move> captures)
+    {
+        Span<int> scores = stackalloc int[captures.Length];
+
+        for (var i = 0; i < captures.Length; i++)
+            scores[i] = CaptureScore(captures[i]);
+
+        for (var i = 0; i < captures.Length; i++)
+        {
+            var move = captures[i];
             var score = scores[i];
 
             var j = i - 1;
             while (j >= 0 && scores[j] < score)
             {
-                moves[j + 1] = moves[j];
+                captures[j + 1] = captures[j];
                 scores[j + 1] = scores[j];
                 j--;
             }
 
-            moves[j + 1] = move;
+            captures[j + 1] = move;
             scores[j + 1] = score;
         }
     }
-
-    private int EvaluateMove(TTEntry? ttEntry, Move move)
-    {
-        var score = 0;
-
-        if (move == ttEntry?.BestMove)
-            score = 1000000;
-
-        if (move.IsCapture)
-            score += 10000 + _pieceValues[(int) move.CapturePieceType] * 100 - _pieceValues[(int) move.MovePieceType];
-        
-        return score;
-    }
-
+    
     private int RelativeEvaluate(Board board)
     {
         var eval = EvaluateBoard(board);
@@ -212,6 +248,20 @@ public class EthanBot : IChessBot
         return CountSetBits(bitboard) * _pieceValues[(int)pieceType];
     }
     
+    private int CaptureScore(Move move)
+    {
+        return _pieceValues[(int)move.CapturePieceType] * 10
+               - _pieceValues[(int)move.MovePieceType];
+    }
+    
+    private (ulong zKey, int ttIndex, TTEntry entry) GetZobrist(Board board)
+    {
+        var zKey = board.ZobristKey;
+        var ttIndex = (int)(zKey & (TTSize - 1));
+        var entry = _tt[ttIndex];
+        return (zKey, ttIndex, entry);
+    }
+    
     private static int CountSetBits(ulong n)
     {
         var count = 0;
@@ -222,14 +272,6 @@ public class EthanBot : IChessBot
         }
         return count;
     }
-    
-    private (ulong zKey, int ttIndex, TTEntry entry) GetZobrist(Board board)
-    {
-        var zKey = board.ZobristKey;
-        var ttIndex = (int)(zKey & (TTSize - 1));
-        var entry = _tt[ttIndex];
-        return (zKey, ttIndex, entry);
-    }
 
     private static int Negate(int i)
     {
@@ -239,5 +281,10 @@ public class EthanBot : IChessBot
             int.MaxValue => int.MinValue,
             _ => -i
         };
+    }
+    
+    private static void Swap(ref Move a, ref Move b)
+    {
+        (a, b) = (b, a);
     }
 }
